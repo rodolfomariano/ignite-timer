2953d9ffd1610f85aff902912b5e7309e280093b
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2022-07-15 09:50:42 -0300

    add styles in the Header component and add links to pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,22 @@
+import { Timer, Scroll } from 'phosphor-react'
+
+import { HeaderContainer } from './styles'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="Imagem de dois triângulos" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
